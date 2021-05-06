f75069555254d99c1a459616cb2697c1b14b806f
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:17:50 -0700

    Corrects underpin autoloader

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 
 	if ( ! defined( 'UNDERPIN_WIDGETS_ROOT_DIR' ) ) {
 		define( 'UNDERPIN_WIDGETS_ROOT_DIR', plugin_dir_path( __FILE__ ) );
@@ -12,7 +12,7 @@ add_action( 'underpin/before_setup', function ( $file ) {
 	require_once( UNDERPIN_WIDGETS_ROOT_DIR . 'lib/abstracts/Widget.php' );
 	require_once( UNDERPIN_WIDGETS_ROOT_DIR . 'lib/loaders/Widgets.php' );
 	require_once( UNDERPIN_WIDGETS_ROOT_DIR . 'lib/factories/Widget_Instance.php' );
-	Underpin\underpin()->get( $file )->loaders()->add( 'widgets', [
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'widgets', [
 		'registry' => 'Underpin_Widgets\Loaders\Widgets',
 	] );
-} );
+}, 10, 2 );
