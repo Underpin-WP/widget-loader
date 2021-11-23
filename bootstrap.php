d1ca1bf9b7cc7e8bd800537257e9d636437b03d8
Alex Standiford <a@alexstandiford.com>
2021-11-23 10:42:10 -0600

    PSR4 support

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+use Underpin\Abstracts\Underpin;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Add this loader.
+Underpin::attach( 'setup', new \Underpin\Factories\Observers\Loader( 'widgets', [
+	'class' => 'Underpin\Widgets\Loaders\Widgets',
+] ) );
